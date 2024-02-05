49f67238021d533f89e4b281ed018dbe003a3aa4
Mailen Lorena Alvarez <m.labarrere2@gmail.com>
2024-02-05 20:55:18 -0300

    Finaliza el ejercicio 01 entrada_salida

--- 00-Unidades/01_entrada_salida/Ejercicios_entrada_salida/ES_app_01.py
+++ 00-Unidades/01_entrada_salida/Ejercicios_entrada_salida/ES_app_01.py
@@ -5,8 +5,9 @@ from tkinter.simpledialog import askstring as prompt
 import customtkinter
 
 '''
-nombre:
-apellido:
+nombre: Mailen Lorena
+apellido: Alvarez
+tutor: Alejandro
 ---
 Ejercicio: entrada_salida_01
 ---
